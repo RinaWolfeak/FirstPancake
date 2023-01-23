aa654ecc3ec23fa464656ce930ecffb505806995
RinaWolfeak <117139822+RinaWolfeak@users.noreply.github.com>
2023-01-23 17:12:49 +0000

    22_1

--- app/models.py
+++ app/models.py
@@ -7,6 +7,8 @@ from app import db, login
 from time import time
 import jwt
 import json
+import redis
+import rq
 
 @login.user_loader
 def load_user(id):
@@ -39,6 +41,7 @@ class User(UserMixin, db.Model):
                                         foreign_keys='Message.recipient_id',
                                         backref='recipient', lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    tasks = db.relationship('Task', backref='user', lazy='dynamic')
 
     def follow(self, user):
         if not self.is_following(user):
@@ -134,3 +137,20 @@ class Notification(db.Model):
     def get_data(self):
         return json.loads(str(self.payload_json))
 
+class Task(db.Model):
+    id = db.Column(db.String(36), primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    description = db.Column(db.String(128))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    complete = db.Column(db.Boolean, default=False)
+
+    def get_rq_job(self):
+        try:
+            rq_job = rq.job.Job.fetch(self.id, connection=current_app.redis)
+        except (redis.exceptions.RedisError, rq.exceptions.NoSuchJobError):
+            return None
+        return rq_job
+
+    def get_progress(self):
+        job = self.get_rq_job()
+        return job.meta.get('progress', 0) if job is not None else 100
